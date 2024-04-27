6bd2b6cb16a66fe94d215ab683666563d1521a39
Christian Debray <christian.debray5@gmail.com>
2024-04-27 23:45:33 +0200

    add command line arguments and help to the main script and set default parameters

--- README.md
+++ README.md
@@ -9,9 +9,19 @@ For the moment, book URL is hard-coded in scrapebooks.py
 ## Usage
 once all packages and dependencies have been installed in a vritual environment:
 
-    `$ python scrapebooks.py`
+`$ python scrapbooks.py [options] [url_to_scrape]`
 
-Reads book data from https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html
-and appends the scraped data to data/test_book.csv
+### Command line arguments:
 
-If the file doesn't exist, creates the file and adds the CSV headers before appending the data.
+  - **scrape_url:**
+    an URL to scrape. see mode option as for how the URL will be interpreted.
+
+options:
+| option | description | default |
+|--------|-------------|---------|
+| -h, --help | show this help message and exit | |
+| -o OUTPUT, --output OUTPUT | specifies path to the output file. | If empty, the file will be stored in the "data" subdir and and named after the input url/file with a timestamp. |
+| --mode {single,category,all}, -m {single,category,all} | scrape mode: single page, all pages of the same category, or all pages from the same base URL. | https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html |
+| --file FILE, -f FILE | for debugging purposes: sets a local HTML file as input, handled as a single scraping. This setting will ignore all others except ouptut. | |
+
+If the output file doesn't exist, creates the file and adds the CSV headers before appending the data.
--- scrapebooks.py
+++ scrapebooks.py
@@ -1,24 +1,91 @@
 from remotedatasource import RemoteDataSource
 from bookdatareader import BookDataReader
 from bookdatawriter import BookDataWriter
+import argparse
+import urllib.parse
+import datetime
+import os
+
+def create_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="scrapbooks",
+        usage="scrapbooks [options] [url_to_scrape]",
+        description="scraps book data found on given URL.")
+    parser.add_argument(
+        "scrape_url",
+        nargs='?',
+        default= "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html",
+        help= "URL to scrape. see mode option as for how the URL will be interpreted."
+        )
+    parser.add_argument(
+        "-o", "--output",
+        required=False,
+        default="",
+        help= "specifies path to the output file."
+        )
+    parser.add_argument(
+        "--mode",
+        "-m",
+        choices=['single', 'category', 'all'],
+        default="single",
+        help="scrape mode: single page, all pages of the same category, or all pages from the same base URL."
+        )
+    parser.add_argument(
+        "--file",
+        "-f",
+        default="",
+        help="for debugging purposes: sets a local HTML file as input, handled as a single scraping. This setting will ignore all others except ouptut."
+    )
+
+    return parser
+
+def gen_output_file_name(scrape_url: str, extension: str= "csv") -> str:
+    """
+    generates a filename for the output csv, consistent with the scraped url and with a timestamp.
+    """
+    parts = urllib.parse.urlsplit(scrape_url)
+    host = parts.netloc
+    path = parts.path.replace('index.html', '')
+    basename = host + '_' + path.strip('/').replace('/', '_')
+    return append_timestamp(basename) + extension
+
+def append_timestamp(in_str: str):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    return in_str + '_' + now.strftime("%Y-%m-%d_%H-%M-%S")
 
 if __name__ == "__main__":
-    book_url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-    csv_output_file = "data/test_book.csv"
-    # source = RemoteDataSource(book_url)
-    # book_html = source.read_text()
+    parser = create_arg_parser()
+    args = parser.parse_args()
+    if not args.file:
+        scrape_url = args.scrape_url
+        input_file = ''
+        csv_output_file = args.output or ("./data/" + gen_output_file_name(scrape_url, '.csv'))
+    else:
+        scrape_url = ''
+        input_file = args.file
+        csv_output_file = args.output or ("./data/" + append_timestamp(os.path.basename(args.file)) + ".csv")
+
+    print("scrape {0}".format(scrape_url or input_file))
+
+    if scrape_url:
+        # read book data from remote source...
+        source = RemoteDataSource(scrape_url)
+        book_html = source.read_text()
+    elif input_file:
+        # debugging with a local file...
+        with open(input_file) as f:
+            book_html = f.read()
+    else:
+        # woops...
+        exit("Missing data source. Please specify URL to scrape or input HTML file.")
 
     # with open("./test_data/A-Light-in-the-Attic_Books-to-Scrape-Sandbox.html") as f:
-    with open("./test_data/bs4_parsed_dump.html") as f:
-        book_html = f.read()
     reader = BookDataReader()
     book = reader.read_from_html(book_html)
-    # debug: dump scraped html
-    # with open("./test_data/html_source_dump.html", "wb") as f:
-    #     f.write(book_html)
-    # with open("./test_data/bs4_parsed_dump.html", "w") as f:
-    #     f.write(reader._soup.prettify())
-    book.product_page_url = book_url
+    book.product_page_url = scrape_url
+
+    print("Export data as CSV to {0}".format(csv_output_file))
+
     writer = BookDataWriter(csv_output_file)
     if success := writer.append_data(book):
         print(f"wrote to {csv_output_file}")
