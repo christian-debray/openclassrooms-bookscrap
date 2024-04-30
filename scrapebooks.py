6e9d256d85c6729ff03194545c28a26ab4aa566a
Christian Debray <christian.debray5@gmail.com>
2024-04-30 11:32:03 +0200

    generate urls to scrape entire catalog or category

--- scrapebooks.py
+++ scrapebooks.py
@@ -1,10 +1,19 @@
-from remotedatasource import RemoteDataSource
-from bookdatareader import BookDataReader
-from bookdatawriter import BookDataWriter
+"""
+openclassrooms Python - Project 2
+bookscraper package
+@author Christian Debray - christian.debray@gmail.com
+"""
 import argparse
 import urllib.parse
 import datetime
 import os
+import csv
+import re
+import time
+from remotedatasource import RemoteDataSource
+from bookdatareader import BookDataReader
+from bookdatawriter import BookDataWriter
+from scrapeindex import ScrapeIndex
 
 def create_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -54,6 +63,9 @@ def append_timestamp(in_str: str):
     return in_str + '_' + now.strftime("%Y-%m-%d_%H-%M-%S")
 
 if __name__ == "__main__":
+    #
+    # parse options
+    #
     parser = create_arg_parser()
     args = parser.parse_args()
     if not args.file:
@@ -65,30 +77,56 @@ if __name__ == "__main__":
         input_file = args.file
         csv_output_file = args.output or ("./data/" + append_timestamp(os.path.basename(args.file)) + ".csv")
 
-    print("scrape {0}".format(scrape_url or input_file))
-
-    if scrape_url:
-        # read book data from remote source...
-        source = RemoteDataSource(scrape_url)
-        book_html = source.read_text()
-    elif input_file:
+    if input_file:
         # debugging with a local file...
+        print(f"debugging with local file {input_file}:")
         with open(input_file) as f:
             book_html = f.read()
-    else:
+        reader = BookDataReader()
+        book = reader.read_from_html(book_html)
+        print(book)
+        exit()
+    elif len(scrape_url) == 0:
         # woops...
         exit("Missing data source. Please specify URL to scrape or input HTML file.")
 
-    # with open("./test_data/A-Light-in-the-Attic_Books-to-Scrape-Sandbox.html") as f:
-    reader = BookDataReader()
-    book = reader.read_from_html(book_html)
-    book.product_page_url = scrape_url
+    urls_index = ScrapeIndex()
 
-    if (book.is_valid()):
-        print("Export data as CSV to {0}".format(csv_output_file))
-        writer = BookDataWriter(csv_output_file)
-        if success := writer.append_data(book):
-            print(f"wrote to {csv_output_file}")
+    # if the output file already exists, add all referenced URLs to the set, and mark them as parsed.
+    if os.path.exists(csv_output_file) and os.stat(csv_output_file).st_size > 0:
+        with open(csv_output_file, "r") as f:
+            csv_reader = csv.DictReader(f)
+            for row in csv_reader:
+                if url := row['product_page_url']:
+                    urls_index.mark_url(url)
+    #
+    # Guess the page type from the path:
+    #
+    scrape_url = re.sub(r'/(index.[a-z]{2,4})?$', '', scrape_url) + '/'
+    if scrape_url in ['https://books.toscrape.com/catalogue/category/books_1/', 'https://books.toscrape.com/']:
+        print("scrape the entire catalog")
+        urls_index.load_generator_from_url(scrape_url)
+    elif re.match(r'^https://books.toscrape.com/catalogue/category/books/[a-zA-Z0-9\-_]+/$', scrape_url):
+        print("scrape a category")
+        urls_index.load_generator_from_url(scrape_url)
     else:
-            print("Invalid book data, skip record.")
+        print("scrape a single page")
+        urls_index.load_generator_from_list([scrape_url])
+
+    data_source = RemoteDataSource()
+    reader = BookDataReader()
+    writer = BookDataWriter(csv_output_file)
+
+    for url in urls_index.list_urls_to_scrape():
+        print(f"scrape {url}")
+        data_source.set_source(url)
+        book_html = data_source.read_text()
+        if book := reader.read_from_html(book_html):
+            book.product_page_url = url
+            if (book.is_valid()):
+                if success := writer.append_data(book):
+                    print(f"exported book data to csv file")
+            else:
+                print("Invalid book data, skip record.")
+        time.sleep(2)
     print("done.")
---
+++ scrapeindex.py
@@ -0,0 +1,99 @@
+"""
+openclassrooms Python - Project 2
+bookscraper package
+@author Christian Debray - christian.debray@gmail.com
+"""
+from remotedatasource import RemoteDataSource
+import urllib.parse
+from bs4 import BeautifulSoup
+from typing import Generator
+from collections.abc import Generator
+
+class ScrapeIndex:
+    """
+    Iterate over URLs to scrap and keep track of URLS already scrapped.
+
+    The list of URLs to scrape will be generated:
+      - either from a list (see the load_generator_from_list() method)
+      - or from a URL where the list of URLs to scrape can be extracted (see the load_generator_from_url() method)
+
+    The load_generator_from_url() supports paginated content and will lazily load the contents of the next page on demand.
+
+    Usage:
+     1. create a new instance of the ScrapeIndex
+     2. load the URL generator
+     3. iterate over the URLs with the list_urls_to_scrape() method
+
+    ```
+     scrape_idx = ScrapeIndex()
+     scrape_idx.load_generator_from_url('https://example.com/path/to/index')
+     for url in scrape_idx.list_urls_to_scrape():
+        # do something brilliant with the url string
+        (...)
+    ```
+    """
+
+    def __init__(self):
+        self.index_url = ''
+        self._url_map: dict[str, str|bool] = {}
+        self._url_generator = self.load_generator_from_list([])
+        self.src = RemoteDataSource()
+
+    def mark_url(self, url, scraped: bool = True):
+        """
+        Marks a url as scraped.
+        """
+        self._url_map[url] = scraped
+
+    def is_scraped(self, url: str) -> bool:
+        """
+        Returns True if a URL is already amrked as being scraped,
+        False otherwise.
+        """
+        return self._url_map.get(url, False)
+
+    def load_generator_from_list(self, url_list: list[str]):
+        """
+        Loads the list of URLs to scrape from a list.
+        """
+        self._url_generator = iter(url_list)
+
+    def load_generator_from_url(self, index_url: str):
+        """
+        Loads the list of URLs to scrape from data found at a given URL.
+        """
+        self._url_generator = iter(self._read_url_index(index_url))
+    
+    def list_urls_to_scrape(self) -> Generator[str]:
+        """
+        Lazily lists the URLs that have not been scraped yet.
+        """
+        for next_url in self._url_generator:
+            if False == self._url_map.get(next_url, False):
+                self.mark_url(next_url)
+                yield next_url
+
+    def _read_url_index(self, index_url) -> Generator[str]:
+        """
+        Reads the contents found at index url and extracts a list of urls to scrape.
+        Lazily proceeds to the next page if content is paginated.
+        """
+        next_index_url = index_url
+        while next_index_url:
+            index_html = self.src.read_text(next_index_url)
+            index_soup = BeautifulSoup(index_html, 'html.parser')
+            if items := index_soup.css.select('section ol.row > li article.product_pod h3 a'):
+                url_list = [urllib.parse.urljoin(next_index_url, link.attrs.get('href', '')) for link in items]
+            else:
+                url_list = []
+            print("Found {0} links".format(len(url_list)))
+            if next_link := index_soup.css.select_one('.pager .next > a'):
+                next_index_url = urllib.parse.urljoin(next_index_url, next_link.attrs['href'])
+            else:
+                next_index_url = ''
+            for url in url_list:
+                yield url
+            if next_index_url:
+                print(f"Proceed to next page: {next_index_url}")
+            else:
+                print("Reached the end of index")
